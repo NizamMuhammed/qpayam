d506c2bbb50d90eb9bb4d54507b40f799c01de16
NizamMuhammed <mohammednizampn@gmail.com>
2021-12-12 20:42:25 +0530

    Homepage created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 import Home from "./pages/Home"
 import GlobalStyle from "./globalStyles"
+import ProductList from "./pages/ProductList"
 
 function App() {
   return (
     <div>
       <GlobalStyle />
-      <Home />
+      <ProductList />
     </div>
   )
 }
