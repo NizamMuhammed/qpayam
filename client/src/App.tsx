e5c2b0fb54ebac593be992e3da58d0f24ffc0000
NizamMuhammed <mohammednizampn@gmail.com>
2021-12-13 09:01:33 +0530

    typo correction

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react"
-//import Home from "./pages/Home"
+import Home from "./Pages/Home"
 import GlobalStyle from "./globalStyles" /* Global Styles defined for the entire App*/
 import Login from "./Pages/Login"
-//import ProductList from "./pages/ProductList"
-//import Product from "./pages/Product"
+import ProductList from "./Pages/ProductList"
+import Product from "./Pages/Product"
 
 function App() {
   return (
     <div>
       <GlobalStyle />
       <Login />
+      <Home />
+      <ProductList />
+      <Product />
     </div>
   )
 }
